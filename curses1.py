e4fcea33010574ac642a34efd77d6bb1845c66b0
BalthazarLlv <112860033+BalthazarLlv@users.noreply.github.com>
2023-01-18 10:33:38 +0000

    Codage des contrôles de jeu

--- curses1.py
+++ curses1.py
@@ -21,10 +21,10 @@ def affichage_aire_de_jeu(hauteur, largeur, titre):
     # La saisie de caractère est non bloquante
     win.nodelay(1)
     # La fenètre a une bordure standard
-    win.____
+    win.box()
     # Définition d'une couleur pour le titre : texte en rouge sur fond blanc
     # Voir dans la documentation la table "lists the predefined colors"
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
     # Affichage du titre
     win.addstr(0, 27, titre, curses.color_pair(1))
     # Raffraichissement de la fenêtre
@@ -48,11 +48,11 @@ titre =['  _______     _________ _    _  ____  _   _    _____ _   _          _  
 # Affichge du titre pendant 2s avant l'ouverture de la fenêtre de jeu
 affichage_titre(titre)
 # Initialisation du terminal
-curses.initscr
+curses.initscr()
 # Démarrage du mode couleur
 curses.start_color()
 # Affichage de l'aire de jeu
-affichage_aire_de_jeu(20,60, 'SNAKE')
+affichage_aire_de_jeu(20,60,'SNAKE')
 # Tempo 10s
 curses.napms(10000)
 # fin de l'affichage de la fenêtre
